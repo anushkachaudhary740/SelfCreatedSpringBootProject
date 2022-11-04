3585b44d7a37dcdd54f7330a9b484954406d4bc2
anushkachaudhary740 <anushka.chaudhary@cognologix.com>
2022-11-04 12:21:11 +0530

    Created whole project by self

--- src/main/java/com/example/SimpleSpringBootProject/controller/BookController.java
+++ src/main/java/com/example/SimpleSpringBootProject/controller/BookController.java
@@ -1,17 +1,36 @@
 package com.example.SimpleSpringBootProject.controller;
 
 import com.example.SimpleSpringBootProject.model.Book;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.SimpleSpringBootProject.services.BookServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class BookController {
+    @Autowired
+    private BookServices bookServices;
     @GetMapping("/get")
-    public Book restBook(){
-        Book book=new Book();
-        book.setId(2);
-        book.setName("Haven");
-        book.setAuthor("HarSwaroop");
-        return book;
+    public List<Book> restBook(){
+        return this.bookServices.getAllBook();
+    }
+    @GetMapping("/get/{id}")
+    public Book restBook(@PathVariable("id") int id){
+        return this.bookServices.getBookById(id);
+    }
+    @PostMapping("/post")
+    public Book addBook(@RequestBody Book book){
+          this.bookServices.postBook(book);
+          return book;
+    }
+    @DeleteMapping("/delete/{id}")
+    public void deleteBook(@PathVariable("id") int id){
+        this.bookServices.deleteBookById(id);
+    }
+    @PutMapping("/put/{id}")
+    public Book restBook(@RequestBody Book book,@PathVariable("id") int id){
+         this.bookServices.updateBookById(book,id);
+         return book;
     }
 }
